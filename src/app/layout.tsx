e41a4ab4be7ce0cc0de2eb5ffba70782ea34c64d
Animesh Kumar <animesh.kumar9651@gmail.com>
2026-07-30 00:09:32 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import GoogleTranslate from '@/components/GoogleTranslate'
+import Script from 'next/script'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function RootLayout({
   children,
@@ -12,10 +14,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Script src="https://checkout.razorpay.com/v1/checkout.js" strategy="lazyOnload" />
         <Navbar />
         <GoogleTranslate />
         {children}
         <Footer />
+
+        {/* Replace G-XYZ with your client's actual Measurement ID */}
+        <GoogleAnalytics gaId="G-VQJ7Q2H4MP" />
       </body>
     </html>
   )
